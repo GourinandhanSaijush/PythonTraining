29aa9cf72760be4cdc1aee53acdbacdda1e70cab
Gourinandhan Saijush <111700929+GourinandhanSaijush@users.noreply.github.com>
2024-09-10 01:48:06 +0530

    Add files via upload

---
+++ training2.py
@@ -0,0 +1,188 @@
+#Simple Calculator using python program 
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
+        
+
+# Python Program to find the L.C.M. of two input number
+
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = 54
+num2 = 24
+
+print("The L.C.M. is", compute_lcm(num1, num2))
+
+
+# Program to check if a number is prime or not using flag method
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
+        
+
+
+#Programs to print triangles using *, numbers and characters
+#Program to print half pyramid using *
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print()
+    
+#Program to print half pyramid a using numbers
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print()
+    
+#Program to print half pyramid using alphabets
+rows = int(input("Enter number of rows: "))
+
+ascii_value = 65
+
+for i in range(rows):
+    for j in range(i+1):
+        alphabet = chr(ascii_value)
+        print(alphabet, end=" ")
+    
+    ascii_value += 1
+    print()
+    
+#Programs to print inverted half pyramid using * and numbers
+#Inverted half pyramid using *
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("* ", end=" ")
+    
+    print()
+    
+#Inverted half pyramid using numbers
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    
+    print()
+    
+#Programs to print full pyramids
+#Program to print full pyramid using *
+rows = int(input("Enter number of rows: "))
+
+k = 0
+
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+   
+    while k!=(2*i-1):
+        print("* ", end="")
+        k += 1
+   
+    k = 0
+    print()
+    
+#Inverted full pyramid of *
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 1, -1):
+    for space in range(0, rows-i):
+        print("  ", end="")
+    for j in range(i, 2*i-1):
+        print("* ", end="")
+    for j in range(1, i-1):
+        print("* ", end="")
+    print()
